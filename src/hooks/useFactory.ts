ea1f0212937c4a3432fa65dedf40472098ec1b1b
JuanSanchez420 <juansanchez420@protonmail.com>
2023-02-05 18:46:43 -0500

    pings API on pool creation

--- src/hooks/useFactory.ts
+++ src/hooks/useFactory.ts
@@ -1,9 +1,11 @@
-import { useCallback, useState } from "react"
+import { useCallback, useContext, useState } from "react"
+import { Global } from "../context/GlobalContext"
 import { CreateState } from "../info/types"
 import { useFactoryContract } from "./useContract"
 
 const useFactory = () => {
   const contract = useFactoryContract()
+  const {network} = useContext(Global)
   const [state, setState] = useState<CreateState>(CreateState.DOESNT_EXIST)
 
   const deploy = useCallback(
@@ -15,6 +17,7 @@ const useFactory = () => {
         const receipt = await tx.wait()
 
         r = receipt.events[2].args.tidePool
+        await fetch(`/poolcreated?address=${r}&network=${network.name}`)
         setState(CreateState.DONE)
       } catch (e) {
         console.log(e)
@@ -22,7 +25,7 @@ const useFactory = () => {
       }
       return r
     },
-    [contract]
+    [contract, network]
   )
 
   return {
